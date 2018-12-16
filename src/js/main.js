9fa6843a41d017452f39dc00079c668024e9bafe
Shuraster <sanya.chernikova@yandex.ru>
2018-12-16 23:27:02 +0300

    add test task

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/slick-carousel/slick/slick.js
+//= ../../bower_components/slickNav/jquery.slicknav.js
+
+/*
+ * Custom
+ */
+//= partials/app.js
